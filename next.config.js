477c5008f79e03c6cf113311a5174a99b8e3f735
moozart-tech <mauroserralvo@gmail.com>
2023-09-17 18:34:43 +0200

    main

--- next.config.js
+++ next.config.js
@@ -2,9 +2,4 @@
 const nextConfig = {}
 
 module.exports = nextConfig
-module.export = require('babelrc')
-{
-    "presets" ["next/babel"],
-    "plugins" [["styled-components", { "ssr": true, "displayName": true }]]
-  };
-swcMinify: true
+
