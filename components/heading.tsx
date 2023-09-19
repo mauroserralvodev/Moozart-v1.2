ec7736a6e545234c8bfe19d7435df421dab91e45
moozart-tech <mauroserralvo@gmail.com>
2023-09-19 19:05:44 +0200

    main

--- components/heading.tsx
+++ components/heading.tsx
@@ -20,7 +20,7 @@ export const Heading = ({
   return (
     <>
       <div className="px-4 lg:px-8 flex items-center gap-x-3 mb-8">
-        <div className={cn("p-2 w-fit rounded-md", bgColor)}>
+        <div className={cn("p-2 w-fit rounded-lg", bgColor)}>
           <Icon className={cn("w-10 h-10", iconColor)} />
         </div>
         <div>
