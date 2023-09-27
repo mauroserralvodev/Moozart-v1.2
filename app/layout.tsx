e4ee517a2f4f6da04189d7b119aff5292401c18b
moozart-tech <mauroserralvo@gmail.com>
2023-09-27 19:43:56 +0200

    main

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
-import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +18,6 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-      <Analytics />
         <body className={inter.className}>{children}</body>
       </html>
     </ClerkProvider>
