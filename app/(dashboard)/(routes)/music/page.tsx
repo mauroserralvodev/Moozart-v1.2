1e44d0d76349af5e7b1201ba14c2cfc3c1b03b29
moozart-tech <mauroserralvo@gmail.com>
2023-10-04 18:47:00 +0200

    main

--- app/(dashboard)/(routes)/music/page.tsx
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -66,14 +66,14 @@ const MusicPage = () => {
         <Form {...form}>
           <form 
             onSubmit={form.handleSubmit(onSubmit)} 
-            className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2">
+            className="bg-gray-100 rounded-lg shadow-lg w-full p-4 px-3 md:px-6  grid grid-cols-12 gap-2">
             <FormField
               name="prompt"
               render={({ field }) => (
                 <FormItem className="col-span-12 lg:col-span-10">
                   <FormControl className="m-0 p-0">
                     <Input
-                      className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                      className="border-none outline-none focus-visible:ring-0 focus-visible:ring-transparent bg-gray-100"
                       disabled={isLoading} 
                       placeholder="Describe la música que quieres escuchar" 
                       {...field}
@@ -83,7 +83,7 @@ const MusicPage = () => {
               )}
             />
             <Button className="bg-black col-span-12 lg:col-span-2 w-full" type="submit" disabled={isLoading} size="icon">
-              Crear
+              <h3 className="text-white">Crear</h3>
             </Button>
           </form>
         </Form>
