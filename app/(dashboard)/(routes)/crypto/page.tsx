f120a08f33b102aba1f6346301e6b3dc9d7a436f
moozart-tech <mauroserralvo@gmail.com>
2023-10-06 21:35:58 +0200

    main

--- app/(dashboard)/(routes)/crypto/page.tsx
+++ app/(dashboard)/(routes)/crypto/page.tsx
@@ -4,7 +4,42 @@ import { Empty } from "@/components/ui/empty";
 
 const Error = () => {
   return ( 
-    <Empty label="Sitio web en obras." />
+    <section className="bg-white dark:bg-gray-900">
+    <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+        <a href="https://www.binance.com/es/blog/all/binance-chain-la-blockchain-para-intercambiar-el-mundo-304219301536473088" className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
+            <span className="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 mr-3">Nuevo</span> <span className="text-sm font-medium">Descubre las novedades Blockchain</span> 
+            <svg className="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+        </a>
+        <div className="h-full p-2 pb-2 flex flex-col items-center justify-center relative mb-4">
+          <img className="" src="/amadeuslogo.png" />
+        </div>
+        <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Los Amadeu coins son la criptomoneda de la plataforma, tienen multiples usos como la compra venta en Marketplace. Pronto en Binance.</p>
+        <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+            <a href="#" className="shadow-lg inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-600 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                Saber más
+                <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            </a>
+            <a href="https://www.youtube.com/@moozartai" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                <svg className="shadow-lg mr-2 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
+                Ver video
+            </a>  
+        </div>
+    </div>
+  <section className="bg-white dark:bg-gray-900">
+    <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+        <img className="w-full dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg" alt="dashboard image"/>
+        <img className="w-full hidden dark:block" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg" alt="dashboard image"/>
+        <div className="mt-4 md:mt-0">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">¿Porque usamos Binance?</h2>
+            <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">En Moozart, elegimos utilizar la Binance Smart Chain (BSC) debido a su alta velocidad, bajo costo y compatibilidad con la red Ethereum. La BSC utiliza un mecanismo de consenso de prueba de participaci&oacute;n delegada (DPoS), lo que permite una mayor velocidad y eficiencia en el procesamiento de transacciones. Adem&aacute;s, la BSC tiene una cadena secundaria llamada Binance Chain, que permite una transacci&oacute;n m&aacute;s r&aacute;pida y econ&oacute;mica.</p>
+            <a href="https://www.binance.com/" className="shadow-lg inline-flex items-center text-white bg-blue-600 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900">
+                Get started
+                <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            </a>
+        </div>
+    </div>
+</section>
+</section>
    );
 }
  
