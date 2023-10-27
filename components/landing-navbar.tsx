e13bb927518f266b991706dacc6ea7dc098d3b44
moozart-tech <mauroserralvo@gmail.com>
2023-10-27 18:50:07 +0200

    main

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -14,6 +14,7 @@ export const LandingNavbar = () => {
   const { isSignedIn } = useAuth();
 
   return (
+    
     <nav className="p-4 flex items-center justify-between">
       <Link href="/" className="flex items-center">
         <div className="relative h-10 w-32 mr-10">
@@ -31,5 +32,6 @@ export const LandingNavbar = () => {
         </Link>
       </div>
     </nav>
+    
   )
 }
