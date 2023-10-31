1cbe77c2ef79155c323a10d54e94da404c3100cb
moozart-tech <mauroserralvo@gmail.com>
2023-10-31 11:59:19 +0100

    main

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -10,7 +10,7 @@ export const LandingHero = () => {
   const { isSignedIn } = useAuth();
 
   return (
-<div className="relative isolate overflow-hidden bg-white py-24 sm:py-32 lg:overflow-visible lg:px-0">
+<div className="relative isolate overflow-hidden bg-white py-12 sm:py-32 lg:overflow-visible lg:px-0">
   <div className="absolute inset-0 -z-10 overflow-hidden">
     <svg className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_bottom,white,transparent)]" aria-hidden="true">
       <defs>
