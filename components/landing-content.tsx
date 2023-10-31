22757202cb0c5b2d98ec4ae213be155434c9c233
moozart-tech <mauroserralvo@gmail.com>
2023-10-31 11:54:49 +0100

    main

--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -34,10 +34,10 @@ const testimonials = [
 export const LandingContent = () => {
   return (
 <div className="px-10 pb-20 mt-0">
-<hr className=" border-gray-200 sm:mx-auto dark:border-gray-700" />
+<hr className="stroke-gray-200 sm:mx-auto dark:border-gray-700" />
 <div className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
   <div className="absolute inset-0 -z-10 overflow-hidden">
-    <svg className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]" aria-hidden="true">
+    <svg className="absolute left-[max(50%,25rem)] top-30 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]" aria-hidden="true">
       <defs>
         <pattern id="e813992c-7d03-4cc4-a2bd-151760b470a0" width="200" height="200" x="50%" y="-1" patternUnits="userSpaceOnUse">
           <path d="M100 200V.5M.5 .5H200" fill="none" />
